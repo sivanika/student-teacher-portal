dcbc095b17df0e76117105d5db39b90382440076
sivanika <sivanika2025@gmail.com>
2026-01-21 12:32:31 +0530

    Fix API base URL by adding trailing slash

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "https://student-teacher-backend-2igr.onrender.com",
+  baseURL: "https://student-teacher-backend-2igr.onrender.com/",
 })
 
 API.interceptors.request.use((req) => {
