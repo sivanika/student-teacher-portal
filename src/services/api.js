4f8edf88b7d5b24bea3299581df0248c7e0ed3ea
sivanika <sivanika2025@gmail.com>
2026-01-21 12:35:48 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "https://student-teacher-backend-2igr.onrender.com/",
+  baseURL: "https://student-teacher-backend-2igr.onrender.com/api",
 })
 
 API.interceptors.request.use((req) => {
